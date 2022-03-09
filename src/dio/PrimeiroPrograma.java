f5575952dae9c22807e9790d662c91a62dbbed0a
Thais Menezes <thais.mas@hotmail.com>
2022-03-08 22:09:20 -0300

    first commit

---
+++ src/dio/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        System.out.println("Hello world! " + (a+b));
+    }
+}
